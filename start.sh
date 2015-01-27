be3ad4554952fc06ec981d7a35fa73a9efdfbf8e
David Renshaw <dwrenshaw@gmail.com>
2015-01-27 18:47:06 -0500

    generate new secret key on each restart

--- start.sh
+++ start.sh
@@ -9,6 +9,8 @@ mkdir -p /var/tmp/sockets
 mkdir -p /var/sqlite3
 cp initdb.sqlite3 /var/sqlite3/db.sqlite3
 
+export SECRET_KEY_BASE=`base64 /dev/urandom | head -c 30`
+
 cd lobsters
 ./bin/rails server -p 10000 -e production
 
